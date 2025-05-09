7998bcd78eeaa3d34b8d8966a5e1c99e7b66bedd
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-09 20:43:56 +0200

    first commit

---
+++ Front/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-red-500">¡Hola, Mundo!</h1>
+        <p className="mt-4 text-lg text-gray-700">Esta es una aplicación React con Tailwind CSS</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
